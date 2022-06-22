b9b006bc4d638e95573686b7fea09841626910ea
zay <damilolaoyeniyi13@gmail.com>
2022-06-23 00:43:23 +0100

    new commit

---
+++ name-print.js
@@ -0,0 +1,11 @@
+var name
+var height
+var Country
+name=prompt ("Enter Your Name")
+document.write("<H1>Hey "+ name)
+
+height=prompt ("Enter Your Height")
+document.write("<H1>Your height is "+ height )
+
+Country=prompt ("Enter Your Country")
+document.write("<H1>You come from "+ Country )
